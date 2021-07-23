1d3a93228c277fc0a68c59d8852c77d157382061
Marcos Souza <mrcsz.m@gmail.com>
2021-07-23 16:43:54 -0300

    list manual

--- exercicios/pages/render/repeticao1.jsx
+++ exercicios/pages/render/repeticao1.jsx
@@ -13,7 +13,7 @@ export default function repeticao1() {
     const itens = []
 
     for (let i = 0; i < listaAprovados.length; i++) {
-      itens.push(<li>{listaAprovados[i]}</li>)
+      itens.push(<li key={i}>{listaAprovados[i]}</li>)
     }
 
     return itens
