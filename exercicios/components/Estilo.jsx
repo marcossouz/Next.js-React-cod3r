ec624dcc7408969d2a7329b529031694543500c1
Marcos Souza <mrcsz.m@gmail.com>
2021-07-23 15:05:30 -0300

    usando Estilo
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

---
+++ exercicios/components/Estilo.jsx
@@ -0,0 +1,18 @@
+export default function Estilo(props) {
+
+  const classAplicada = props.numero >= 0 ? "azul" : "vermelho"
+
+  return (
+    <div>
+      <h1 style={{
+        backgroundColor: props.numero >= 0 ? "#2D2" : "#D22",
+        color: props.color,
+        textAlign: props.direita ? "right" : "left"
+        }}>
+        Texto #01
+      </h1>
+      <h2 className={classAplicada} > Texto #02 </h2>
+      <h2 className={props.numero >= 0 ? "azul" : "vermelho"} > Texto #02 </h2>
+    </div>
+  )
+}
