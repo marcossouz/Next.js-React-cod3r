69a125407d7b99c04effb6cdf1a99f346fe18e9d
Marcos Souza <mrcsz.m@gmail.com>
2021-07-24 23:07:44 -0300

    modulo v1.7
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- exercicios/pages/modulo/teste.jsx
+++ exercicios/pages/modulo/teste.jsx
@@ -1,11 +1,11 @@
-import Comp3, { Comp1 as Blabla, Comp2 } from '../../components/modulo/funcionais'
+import Padrao, { Comp1 as Blabla, Comp2 } from '../../components/modulo/funcionais'
 
 export default function teste() {
   return (
     <div>
       <Blabla />
       <Comp2 />
-      <Comp3 />
+      <Padrao />
     </div>
   )
 }
