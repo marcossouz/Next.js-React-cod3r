6f23e1b0e1ee3430480c7f4f2d19a9091b23ee3c
Marcos Souza <mrcsz.m@gmail.com>
2021-08-06 21:24:49 -0300

    Estatico #01
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- renderizacao/pages/estatico1.jsx
+++ renderizacao/pages/estatico1.jsx
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react"
 
 export default function estatico1() {
 
-  const [num, setNum] = useState(0)
+  // const [num, setNum] = useState(0)
 
-  useEffect(() => {
-    setNum(Math.random())
-  }, [])
+  // useEffect(() => {
+  //   setNum(Math.random())
+  // }, [])
 
   return (
     <div>
       <h1>Estatico #01 - Versão 2</h1>
-      <h2>{num}</h2>
+      {/* <h2>{num}</h2> */}
     </div>
   )
 }
