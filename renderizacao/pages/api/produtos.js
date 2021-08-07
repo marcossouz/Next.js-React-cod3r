31dcee755aee15ba911cf530fc301c1a6e8a1e5e
Marcos Souza <mrcsz.m@gmail.com>
2021-08-06 22:51:18 -0300

    Componente Dinâmico #02
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- renderizacao/pages/api/produtos.js
+++ renderizacao/pages/api/produtos.js
@@ -1,10 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+function numeroAleatorio(min = 1, max = 10000) {
+  return parseInt(Math.random() * (max - min)) + min
+}
+
 export default function handler(req, res) {
   res.status(200).json([
-    {id: 1, nome: 'caneta', preco: 5.60},
-    {id: 2, nome: 'caderno', preco: 15.60},
-    {id: 3, nome: 'borracha', preco: 7.30},
-    {id: 4, nome: 'tesoura', preco: 21.55},
+    {id: numeroAleatorio(), nome: 'caneta', preco: 5.60},
+    {id: numeroAleatorio(), nome: 'caderno', preco: 15.60},
+    {id: numeroAleatorio(), nome: 'borracha', preco: 7.30},
+    {id: numeroAleatorio(), nome: 'tesoura', preco: 21.55},
   ])
 }
