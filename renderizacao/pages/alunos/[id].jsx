329473eee1ff0aad4494eb8d7d61631418ac3e4d
Marcos Souza <mrcsz.m@gmail.com>
2021-08-07 09:33:14 -0300

    Usando Fallback True
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -3,21 +3,23 @@ export async function getStaticPaths() {
   const ids = await resp.json()
 
   const paths = ids.map(id => {
-    return { params: { id: `${id}` }}
+    return { params: { id: `${id}` } }
   })
 
   return {
-    fallback: false,
+    fallback: true, // false => 404
     paths
   }
-} 
+}
 
 export async function getStaticProps({ params }) {
   const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
   const aluno = await resp.json()
-  return {props: {
-    aluno
-  }}
+  return {
+    props: {
+      aluno
+    }
+  }
 }
 
 export default function AlunoPorId(props) {
@@ -26,11 +28,14 @@ export default function AlunoPorId(props) {
   return (
     <div>
       <h1>Detalhes do aluno</h1>
-      <ul>
-        <li>{aluno.id}</li>
-        <li>{aluno.nome}</li>
-        <li>{aluno.email}</li>
-      </ul>
+      {aluno ?
+        <ul>
+          <li>{aluno.id}</li>
+          <li>{aluno.nome}</li>
+          <li>{aluno.email}</li>
+        </ul>
+        : false
+      }
     </div>
   )
 }
