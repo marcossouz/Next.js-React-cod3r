823b75349ec6dfba020f82f15b5385d3b96c2302
Marcos Souza <mrcsz.m@gmail.com>
2021-08-06 21:16:37 -0300

    versão 2 - ainda aparece warning
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- renderizacao/pages/estatico1.jsx
+++ renderizacao/pages/estatico1.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
+
 export default function estatico1() {
+
+  const [num, setNum] = useState(Math.random())
+
   return (
     <div>
-      <h1>Estatico #01</h1>
-      <h2>{Math.random()}</h2>
+      <h1>Estatico #01 - Versão 2</h1>
+      <h2>{num}</h2>
     </div>
   )
 }
