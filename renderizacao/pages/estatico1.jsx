fd87a605aa30b87be796d0abeb97d8d76f6d0271
Marcos Souza <mrcsz.m@gmail.com>
2021-08-06 21:19:22 -0300

    solve warning
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- renderizacao/pages/estatico1.jsx
+++ renderizacao/pages/estatico1.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function estatico1() {
 
-  const [num, setNum] = useState(Math.random())
+  const [num, setNum] = useState(0)
+
+  useEffect(() => {
+    setNum(Math.random())
+  }, [])
 
   return (
     <div>
